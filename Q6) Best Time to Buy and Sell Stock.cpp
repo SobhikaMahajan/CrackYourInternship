7c0e5c13dbdc75ddcc23547cae905dce84ba6906
Sobhika Mahajan <88205144+SobhikaMahajan@users.noreply.github.com>
2022-06-05 20:31:09 +0530

    Create Q6) Best Time to Buy and Sell Stock.cpp

---
+++ Q6) Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minSoFar = prices[0];
+        int maxProfit = 0;
+        
+        for(int i=0; i<prices.size(); i++){
+            minSoFar = min(minSoFar,prices[i]);
+            int profit = prices[i]-minSoFar;
+            maxProfit = max(profit,maxProfit);
+        }
+        return maxProfit;
+    }
+};
